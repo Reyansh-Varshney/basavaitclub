1d5545ea2ab2bf3c678f151eacbef97535df85ba
Reyansh Varshney <coderreyansh@gmail.com>
2025-04-10 15:59:53 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,9 +18,9 @@ const Footer = () => {
               Empowering students with technology skills and fostering innovation at Basava International School.
             </p>
             <div>
-              <a href="mailto:itclub@basava.edu.in" className="text-gray-300 hover:text-white flex items-center gap-2">
+              <a href="mailto:itclub@basavainternational.onmicrosoft.com" className="text-gray-300 hover:text-white flex items-center gap-2">
                 <Mail size={20} />
-                <span>itclub@basava.edu.in</span>
+                <span>itclub@basavainternational.onmicrosoft.com</span>
               </a>
             </div>
           </div>
